53071507db5eeedcdbd751de191387c7562a4b06
Koha Choji <koha.choji@vistair.com>
2019-11-01 15:00:20 +0000

    npm install typescript ts-loader --save-dev; config to build

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,14 @@
 module.exports = {
   entry: './src/index.ts',
   devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: 'ts-loader'
+      }
+    ]
+  },
   resolve: {
     extensions: ['.tsx', '.ts', '.js']
   },
